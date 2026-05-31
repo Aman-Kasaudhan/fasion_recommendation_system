e4c17cf17e24a2bf5835c3e5a32d8d2a7c1ddd2a
Aman-Kasaudhan <rajg99842@gmail.com>
2026-05-31 14:08:27 +0530

    Update recommender.py

--- model/recommender.py
+++ model/recommender.py
@@ -5,9 +5,9 @@ import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import os
-import streamlit as st
+# import streamlit as st
 model=ResNet50(weights="imagenet",include_top=False,input_shape=(224,224,3))
 model.trainable=False
 model=tensorflow.keras.Sequential([
